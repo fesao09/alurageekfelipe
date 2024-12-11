a9eb6951fe482008ea46e8f1d056b2a2e8ce14cc
Felipe Oliveira <felipe.oliveira@ZBN.GROUP>
2024-12-11 14:21:03 -0300

    Simplify serverless function configuration for Vercel

--- index.js
+++ index.js
@@ -6,14 +6,4 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
-  console.log('Request received');
-  server.handle(req, res, (err) => {
-    if (err) {
-      console.error('Error occurred:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      console.log('Request handled successfully');
-    }
-  });
-};
+module.exports = server;
